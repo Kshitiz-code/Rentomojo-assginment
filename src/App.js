548a2577735fab5ec50fddac6c901985ad6eb1b6
Kshitiz Gupta <kshitiz@gmail.com>
2022-07-18 00:03:01 +0530

    latest update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import Axios from "axios";
+import styled from 'styled-components';
+import GameComponent from './Components/GameComponent';
+
+const GameList = styled.div`
+flex-direction:;
+display:flex;
+flex-wrap:wrap;
+justify-content:space-evenly;
+padding : 30px;
+cursor:pointer;
+cover
+`;
 
 function App() {
+  const [SearchQuery, UpdateSearchQuery] = useState();
+  const [timeoutID, updateTimeoutID] = useState();
+
+  const fetchData = async (searchString) => {
+    const response = await Axios.get('https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json');
+    console.log(response)
+  }
+
+  const onTextChange = (event) => {
+    clearTimeout(timeoutID);
+    UpdateSearchQuery(event.target.value);
+    const timeout = setTimeout(() => fetchData(event.target.value), 500);
+    updateTimeoutID(timeout);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="header">Games Web Application</header>
+        <div className='searchBox'>
+          <div>
+            <input
+              className='searchInput'
+              placeholder="SEARCH GAME"
+              onChange={onTextChange}
+              value={SearchQuery}>
+            </input>
+            <image src=''></image>
+          </div>
+          <GameList>
+            {GameList?.length ? (GameList.map((game, index) => (<GameComponent key={index} game={game} />))) : "NO GAME AVAILABLE"}
+          </GameList>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
